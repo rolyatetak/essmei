1e4a87ef501442fee9c5ff112a6dfcb01eb55a4d
kate skow <35641557+rolyatetak@users.noreply.github.com>
2020-03-08 16:27:04 -0700

    Add files via upload

---
+++ hi.js
@@ -0,0 +1,23 @@
+console.log('hi')
+
+$('.show-filters').on('click', function() {
+  if ($('.filters-list').is(':visible')) {
+    $('.filters-list').slideUp(300)
+    $('.show-filters').text('Show Filters')
+  } else {
+    $('.filters-list').slideDown(300)
+    $('.show-filters').text('Hide Filters')
+  }
+})
+
+$('.filters-list a').on('click', function() {
+  var filter = $(this).attr('data-filter')
+
+  $('.product').hide()
+  $(filter).show()
+  
+  $('.filters-list a').removeClass('active')
+  $(this).addClass('active')
+  
+  return false
+})
